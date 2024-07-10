4a431283527ff12bf2c00e3b4eb5b0bca28f51e0
YashodhaSamarawickrama <yasho.samarawickrama@gmail.com>
2024-07-10 15:28:23 +0530

    Update checkout step to take ref

--- index.js
+++ index.js
@@ -7,16 +7,28 @@ const execProm = util.promisify(exec);
 async function run() {
 try  {
     const token = core.getInput('token');
-    const serverUrl = core.getInput('serverUrl');
+    const username = core.getInput('username');
     const userOrgName = core.getInput('userOrgName');
     const userRepoName = core.getInput('userRepoName');
     const commitUser = core.getInput('commitUser');
     const commitEmail = core.getInput('commitEmail');
     const configRepoName = core.getInput('configRepoName');
+    const serverUrl = core.getInput('serverUrl');
     const branch = core.getInput('branch');
+    const ref = core.getInput('ref');
     const disableSSL = core.getInput('disableSSL');
 
     console.log("Started removing files in current directory");
+    exec(disableSSL === 'true' ? `git config --global http.sslVerify false` : `git config --global http.sslVerify true`, (err, stdout, stderr) => {
+        if (err) {
+            console.log(err);
+            core.setOutput("choreo-status", "failed");
+            core.setFailed(err.message);
+            return;
+        }
+        console.log(stdout);
+        console.log(stderr);
+        console.log("Completed disabling SSL verification");
     exec(`rm -rf /home/runner/workspace/${configRepoName}/${configRepoName}/*`, (err, stdout, stderr) => {
         if (err) {
             console.log(err);
@@ -27,17 +39,6 @@ try  {
         console.log(stdout);
         console.log(stderr);
         console.log("Completed removing files in current directory");
-        exec(disableSSL === 'true' ? `git config --global http.sslVerify false` : `git config --global http.sslVerify true`, (err, stdout, stderr) => {
-            if (err) {
-                console.log(err);
-                core.setOutput("choreo-status", "failed");
-                core.setFailed(err.message);
-                return;
-            }
-            console.log(stdout);
-            console.log(stderr);
-            console.log("Completed disabling SSL verification");
-        
         console.log("Started checkout to GitLab repo");
         exec(`git config --global --add safe.directory /home/runner/workspace/${configRepoName}/${configRepoName}`, (err, stdout, stderr) => {  
             if (err) {
@@ -68,21 +69,7 @@ try  {
                     }
                     console.log(stdout);
                     console.log(stderr);
-                    console.log("Completed git config user.name and user.email");             
-                    if(serverUrl!=""){
-                        exec(`git config http.extraHeader "Authorization: Bearer ${token}"`,(err,stdout,stderr)=>{
-                            if (err) {
-                                console.log(err);
-                                core.setOutput("choreo-status", "failed");
-                                core.setFailed(err.message);
-                                return;
-                            }
-                            console.log(stdout);
-                            console.log(stderr);
-                            console.log("Completed git config http.extraHeader");
-                        });
-                    }                  
-                    
+                    console.log("Completed git config user.name and user.email");
                     exec(`git remote add origin ${serverUrl}/${userOrgName}/${userRepoName}.git`, (err, stdout, stderr) => {
                         if (err) {
                             console.log(err);
@@ -93,7 +80,7 @@ try  {
                         console.log(stdout);
                         console.log(stderr);
                         console.log("Completed git remote add origin");
-                        exec(`git fetch --all`, (err, stdout, stderr) => {
+                        exec(`git -c protocol.version=2 fetch --no-tags --prune --progress --no-recurse-submodules origin`, (err, stdout, stderr) => {
                             if (err) {
                                 console.log(err);
                                 core.setOutput("choreo-status", "failed");
@@ -103,28 +90,17 @@ try  {
                             console.log(stdout);
                             console.log(stderr);
                             console.log("Completed git fetch");
-                            exec(`git branch --list --remote origin/${branch}`, (err, stdout, stderr) => {
+                            exec(`git checkout -b ${ref} ${ref}`, (err, stdout, stderr) => {
                                 if (err) {
                                     console.log(err);
                                     core.setOutput("choreo-status", "failed");
                                     core.setFailed(err.message);
                                     return;
                                 }
-                                console.log("branches");
-                                console.log("branches" + stdout);
+                                console.log("ref");
+                                console.log("ref" + stdout);
                                 console.log(stderr);
-                                console.log("Completed git branch --list --remote");
-                                exec(`git checkout --progress --force -B ${branch} ${branch}`, (err, stdout, stderr) => {
-                                    if (err) {
-                                        console.log(err);
-                                        core.setOutput("choreo-status", "failed");
-                                        core.setFailed(err.message);
-                                        return;
-                                    }
-                                    console.log(stdout);
-                                    console.log(stderr);
-                                    console.log("Completed checkout to GitLab repo");
-                                });
+                                console.log("Completed checkout to GitLab repo");
                             });
                         });
                     });
